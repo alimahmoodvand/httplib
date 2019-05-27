3e055cda5a299b0fbab8ad629b7d4da5ec2a8062
acer <alimahmudvand@yahoo.com>
2019-05-27 20:46:13 +0430

    Initial commit

---
+++ httplibrary/src/main/java/ir/alimahmoodvand/httplibrary/requestSender.java
@@ -0,0 +1,16 @@
+package ir.alimahmoodvand.httplibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by acer on 5/27/2019.
+ */
+
+public class requestSender {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
