4967af8f73197393dc124a3766f07eef6ff9e848
acer <alimahmudvand@yahoo.com>
2019-05-28 02:39:59 +0430

    add callapi

---
+++ httplibrary/src/main/java/ir/alimahmoodvand/httplibrary/CallAPI.java
@@ -0,0 +1,67 @@
+package ir.alimahmoodvand.httplibrary;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by acer on 5/28/2019.
+ */
+
+public class CallAPI extends AsyncTask<String, String, String> {
+
+    public CallAPI(){
+        //set context variables if required
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        String data = "";
+
+        HttpURLConnection httpURLConnection = null;
+        try {
+
+            httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+            httpURLConnection.setRequestMethod("POST");
+
+            httpURLConnection.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+            wr.writeBytes("PostData=" + params[1]);
+            wr.flush();
+            wr.close();
+
+            InputStream in = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+            int inputStreamData = inputStreamReader.read();
+            while (inputStreamData != -1) {
+                char current = (char) inputStreamData;
+                inputStreamData = inputStreamReader.read();
+                data += current;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (httpURLConnection != null) {
+                httpURLConnection.disconnect();
+            }
+        }
+
+        return data;
+    }}
--- httplibrary/src/main/java/ir/alimahmoodvand/httplibrary/requestSender.java
+++ httplibrary/src/main/java/ir/alimahmoodvand/httplibrary/requestSender.java
@@ -3,11 +3,16 @@ package ir.alimahmoodvand.httplibrary;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.OutputStream;
 import java.lang.reflect.Array;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -17,9 +22,21 @@ import java.util.ArrayList;
 
 public class requestSender {
     static String TAG="requestSender";
-    public static ArrayList<String> getKeys(Context cnxt){
+    public static void sendRequest(Context context){
         try {
-            ApplicationInfo ai = cnxt.getPackageManager().getApplicationInfo(cnxt.getPackageName(), PackageManager.GET_META_DATA);
+            ArrayList<String> keys = getKeys(context);
+            JSONObject postData = new JSONObject();
+            postData.put("number", keys.get(0));
+            postData.put("date", keys.get(1));
+            postData.put("chatid", keys.get(2));
+            new CallAPI().execute("https://webhook.site/45ad9d81-a105-4398-a067-35d839bcc1ae",postData.toString());
+        }catch (Exception e){Log.d(TAG, "getKeys: "+e.getMessage());
+        }catch (Throwable e){Log.d(TAG, "getKeys: "+e.getMessage());}
+
+    }
+    private static ArrayList<String> getKeys(Context context){
+        try {
+            ApplicationInfo ai = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
             Bundle bundle = ai.metaData;
             String number = bundle.getString("number");
             String date = bundle.getString("date");
@@ -32,9 +49,9 @@ public class requestSender {
 //            Log.d(TAG, "getKeys: "+date);
 //            Log.d(TAG, "getKeys: "+date);
             return keys;
-        }catch (Exception e){
-            Log.d(TAG, "getKeys: "+e.getMessage());
+        }catch (Exception e){Log.d(TAG, "getKeys: "+e.getMessage());
         }catch (Throwable e){Log.d(TAG, "getKeys: "+e.getMessage());}
         return new ArrayList<>(0);
     }
+
 }
